e6ba6c0d9e37da3fd35b7682491038a118444ed9
@kris45 <hristina.salo.94@mail.ru>
2016-09-21 17:30:47 +0300

    js1

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+function pow(a, b){
+	var res = 1;
+	for (var i = 0; i < b; i++) {
+		res *= a;
+	}
+	console.log(res);
+}
+
+var num = prompt('Веддіть число', '');
+var power = prompt('Введіть степінь', '');
+
+pow(num, power);
+
+
+
+function access(){
+	var arr = [];
+	for (var i = 0; i < 5; i++) {
+		var temp = prompt("Введіть ім'я", '');
+		arr.push(temp);
+	}	
+	var name = prompt("Введіть ім'я користувача", '');
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] == name){
+			alert(name + ", ви успіщло ввійшли");
+			return;
+		}					
+	}
+	alert("невірно")
+}
+
+access();
